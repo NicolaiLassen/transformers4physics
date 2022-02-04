ecfe6130ebfdbf2201fa21daafb3a924d5a52bf0
Sebastian <sebastian.kotewitz@gmail.com>
2022-02-04 11:40:56 +0100

    Added a test script to quickly see if the conversion to image sitll works. Resolved the point determination when generating hField

--- util/magtense/prism_grid.py
+++ util/magtense/prism_grid.py
@@ -15,14 +15,14 @@ def normalizeVector(vector):
     return vector/np.sqrt(np.sum(vector**2)),np.sqrt(np.sum(vector**2))
 
 # generate simple prism grid to "sim" grains of micro
-def create_prism_grid(rows=2, columns=2, sizeX=1, sizeY=1, sizeZ=1, res=224):
+def create_prism_grid(rows=2, columns=2, size=1, res=224):
     tiles = magtense.Tiles(rows*columns)
     tiles.set_tile_type(2)
-    tiles.set_size([sizeX, sizeY, sizeZ])
+    tiles.set_size([size, size, size])
     for c in range(columns):
         for r in range(rows):
             i = r+c*rows
-            offset = [sizeX/2+r*sizeX,sizeY/2+c*sizeY,0]
+            offset = [size/2+r*size,size/2+c*size,0]
             tiles.set_offset_i(offset,i)
             tiles.set_center_pos_i(offset,i)
             ea = [
@@ -38,26 +38,16 @@ def create_prism_grid(rows=2, columns=2, sizeX=1, sizeY=1, sizeZ=1, res=224):
             tiles.set_remanence_i(1.2/(4*math.pi*1e-7),i)
             tiles.set_M(tiles.u_ea[i]*tiles.M_rem[i],i)
 
-    ## TODO Change points to instead be scaled down pixel placements
-    ## Currently, the points are set to follow the grid as if each pixel in the resulting 
-    # image was the same size as an internal magnet
-    ## This is wrong, it should instead be the points between the magnets that correspond
-    # to the placement of the pixels in the resulting image
-    points = [[sizeX/2+x, sizeY/2+y, 0] for y in range(res) for x in range(res)]
-
-    magtense.run_simulation(tiles,points)
-    hField = magtense.get_H_field(tiles,points)
-
     paddingDim = 0 if rows==columns else 1 if rows < columns else 2
     sideLen = min(res//rows,res//columns)
     if((res-sideLen*(rows if paddingDim == 2 else columns))%2!=0):
-        raise Exception('this setup can not be properly scaled (whole numbers for dimensions not possible)')
+        sideLen = sideLen - 1
     elif((res-sideLen*(columns if paddingDim == 2 else rows))%2!=0):
-        raise Exception('this setup can not be properly scaled (whole numbers for dimensions not possible)')
+        sideLen = sideLen - 1
     outerPadding = (res-sideLen*(rows if paddingDim == 2 else columns))//2
     innerPadding = (res-sideLen*(columns if paddingDim == 2 else rows))//2
-    startX = outerPadding + (innerPadding if paddingDim == 1 else 0)
-    startY = outerPadding + (innerPadding if paddingDim == 2 else 0)
+    startX = innerPadding if paddingDim == 1 else outerPadding
+    startY = innerPadding if paddingDim == 2 else outerPadding
 
     imageIn = np.zeros((res,res,4))
     for c in range(columns):
@@ -75,6 +65,17 @@ def create_prism_grid(rows=2, columns=2, sizeX=1, sizeY=1, sizeZ=1, res=224):
                 startX+sideLen*r:startX+sideLen*(r+1),
                 3,
             ] = lenM
+
+    pixelSize = size/sideLen
+    pointStartX = pixelSize/2-(innerPadding if paddingDim == 1 else outerPadding)*pixelSize
+    pointStartY = pixelSize/2-(innerPadding if paddingDim == 2 else outerPadding)*pixelSize
+    points = np.zeros((res*res,3))
+    for i in range(res):
+        for j in range(res):
+            points[j+i*res,:] = [pointStartX+j*pixelSize, pointStartY+i*pixelSize, 0]
+
+    magtense.run_simulation(tiles,points)
+    hField = magtense.get_H_field(tiles,points)
 
     imageOut = np.zeros((res*res,4))
     normalizedH = [normalizeVector(x)[0] for x in hField]
@@ -113,90 +114,3 @@ def create_dataset(set_size=1024, columns=[4], rows=[4], square_grid=False, res=
         images_in.append(image_in)
         images_target.append(image_target)
     return PrismGridDataset(images_in, images_target)
-
-
-#%%
-a = create_dataset(
-    set_size=5,
-    columns=[4],
-    rows=[4],
-    res=224,
-)
-
-#%%
-class PrismGridDataset(torch.utils.data.Dataset):
-    def __init__(self, datapath, field_input, action_input, target):
-        super(PrismGridDataset, self).__init__()
-        self.db_path = os.path.dirname(os.path.abspath(__file__)) + '/../data/' + datapath
-        self.size = h5py.File(self.db_path, mode='r')['action'].shape[0]
-        self.field_input = field_input
-        self.action_input = action_input
-        self.target = target
-
-    def open_hdf5(self):
-        self.db = h5py.File(self.db_path, mode='r')
-
-    def __getitem__(self, idx):
-        if not hasattr(self, 'db'):
-            self.open_hdf5()
-        
-        field = self.db['field'][idx]
-        symm_mat = self.db['soft_mat'][idx]
-        action = self.db['action'][idx]
-        action_pos = self.db['id_pos'][idx]
-        p2p = self.db['p2p'][idx] * 1e6
-        p2p_next = self.db['p2p_next'][idx] * 1e6
-        sph_ampl = self.db['sph_harm'][idx]
-        sph_ampl_next = self.db['sph_harm_next'][idx]
-        
-        field = torch.from_numpy(field.astype('float32'))
-        p2p= torch.from_numpy(p2p.astype('float32'))
-        p2p_next = torch.from_numpy(p2p_next.astype('float32'))
-        
-        # Field pre-processing
-        if self.field_input == 'field':
-            field_repr = field
-        elif self.field_input == 'spherical_harmonics':
-            field_repr = sph_ampl
-        else:
-            raise NotImplementedError()
-
-        # Action pre-processing
-        if self.action_input == 'index':
-            a_input = action
-        elif self.action_input == 'position':
-            a_input = torch.from_numpy(action_pos.astype('float32'))
-        elif self.action_input == 'one_hot':
-            z = action[0] // 8
-            y_dict = {0:1, 1:2, 2:0, 3:3, 4:0, 5:3, 6:1, 7:2}
-            y = y_dict[action[0] % 8]
-            x = (action[0] % 8) // 2
-            a_input = (x, y, z)
-        else:
-            raise NotImplementedError()
-
-        # Target pre-processing
-        if self.target == 'p2p_next':
-            target = p2p_next
-        elif self.target == 'bins':
-            p2p_diff = p2p - p2p_next
-            if p2p_diff < -250:
-                target = 0
-            elif (p2p_diff > -250) and (p2p_diff <= -50):
-                target = 1
-            elif (p2p_diff > -50) and (p2p_diff <= 50):
-                target = 2
-            elif (p2p_diff > 50) and (p2p_diff <= 250):
-                target = 3
-            elif (p2p_diff > 250):
-                target = 4
-
-        return field_repr, a_input, target
-
-    def __len__(self):
-        return self.size
-
-if __name__ == '__main__':
-    create_prism_grid()
-
-#%%
---
+++ util/magtense/val_test.py
@@ -0,0 +1,31 @@
+#%%
+import numpy as np
+from prism_grid import create_prism_grid
+
+def testImageIn():
+    i,_ = create_prism_grid(
+        rows=1,
+        columns=2,
+        res=6,
+    )
+    assert np.all(i[0,:,:] == 0)
+    assert np.all(i[:,0:2,:] == 0)
+    assert np.all(i[:,4:6,:] == 0)
+    assert np.all(i[1:3,2:4,:] == i[1,2,:])
+    assert np.all(i[3:5,2:4,:] == i[3,2,:])
+
+    i,_ = create_prism_grid(
+        rows=2,
+        columns=2,
+        res=4,
+    )
+    assert np.all(i[0:2,0:2,:] == i[0,0,:])
+    assert np.all(i[0:2,2:4,:] == i[0,2,:])
+    assert np.all(i[2:4,0:2,:] == i[2,0,:])
+    assert np.all(i[2:4,2:4,:] == i[2,2,:])
+
+
+
+    print('done')
+
+testImageIn()
