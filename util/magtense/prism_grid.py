905b980684c227c7cb3ba91872cf3d20009541c6
Sebastian Tønne Kotewitz <sebastian.kotewitz@gmail.com>
2022-02-07 16:11:54 +0100

    merge

--- util/magtense/prism_grid.py
+++ util/magtense/prism_grid.py
@@ -102,8 +102,8 @@ def create_prism_grid(rows=2, columns=2, size=1, res=224):
     # Back to tesla
     imageOut[3, :, :] = imageOut[3, :, :]*(4*math.pi*1e-7)
 
-    imageOut[3,:,:] = imageOut[3,:,:]*(4*math.pi*1e-7)
-    imageIn[3,:,:] = imageIn[3,:,:]*(4*math.pi*1e-7)
+    imageOut[3, :, :] = imageOut[3, :, :]*(4*math.pi*1e-7)
+    imageIn[3, :, :] = imageIn[3, :, :]*(4*math.pi*1e-7)
 
     return imageIn, mask, imageOut
 
@@ -148,6 +148,7 @@ def create_dataset(set_size=1024, columns=[4], rows=[4], square_grid=False, res=
         masks.append(mask)
         images_target.append(image_target)
     return PrismGridDataset(images_in, mask, images_target)
+
 
 # %%
 a = create_dataset(set_size=64, columns=[2], rows=[2], res=2)
