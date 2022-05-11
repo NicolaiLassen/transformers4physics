fixed normalization issues with embedder, now training transformer

asd.py
@@ -3,7 +3,12 @@ from matplotlib import pyplot as plt
 import numpy as np
 
 f = h5py.File('losses.h5', 'r')
-losses = np.array(f['losses'])
+losses = np.array(f['train'])
+l = np.arange(len(losses))
+plt.plot(l,losses)
+plt.yscale('log')
+plt.show()
+losses = np.array(f['val'])
 l = np.arange(len(losses))
 plt.plot(l,losses)
 plt.yscale('log')

embedding/embedding_landau_lifshitz_gilbert.py
@@ -151,6 +151,12 @@ class LandauLifshitzGilbertEmbedding(EmbeddingModel):
         """
         out = self.backbone.recover(g)
         x = self._unnormalize(out)
+        if(not self.training):
+            normsX = torch.sqrt(torch.einsum('ij,ij->j',x.swapaxes(1,3).reshape(-1,3).T, x.swapaxes(1,3).reshape(-1,3).T))
+            normsX = normsX.reshape(-1,16,64).swapaxes(1,2)
+            x[:,0,:,:] = x[:,0,:,:]/normsX
+            x[:,1,:,:] = x[:,1,:,:]/normsX
+            x[:,2,:,:] = x[:,2,:,:]/normsX
         return x
 
     def koopman_operation(self, g: Tensor, field: Tensor) -> Tensor:
@@ -201,7 +207,7 @@ class LandauLifshitzGilbertEmbedding(EmbeddingModel):
 
     def _unnormalize(self, x: Tensor) -> Tensor:
         x = self.std[:3].unsqueeze(0).unsqueeze(-1).unsqueeze(-1) * x + self.mu[:3].unsqueeze(0).unsqueeze(-1).unsqueeze(-1)
-        x = F.normalize(x,p=2,dim=1)
+        # x = F.normalize(x,p=2,dim=1)
         return x
 
     def _normalize_features(self, field):
@@ -216,14 +222,16 @@ class LandauLifshitzGilbertEmbeddingTrainer(EmbeddingTrainingHead):
         l1: Penalty weight for the reconstruction
         l2: Penalty weight for the dynamics in the koopman sequences
         l3: Penalty weight for the decay of the koopman operator, prevents overfitting
+        l4: Penalty weight for ensuring unit vectors in the output
     """
 
-    def __init__(self, embedding_model: EmbeddingModel, l1=1, l2=1e3, l3=1e-2):
+    def __init__(self, embedding_model: EmbeddingModel, l1=1, l2=1e3, l3=1e-2, l4=1):
         super().__init__()
         self.embedding_model = embedding_model
         self.l1 = l1
         self.l2 = l2
         self.l3 = l3
+        self.l4 = l4
 
     def forward(self, states: Tensor, field: Tensor) -> FloatTuple:
         """Trains model for a single epoch
@@ -264,8 +272,11 @@ class LandauLifshitzGilbertEmbeddingTrainer(EmbeddingTrainingHead):
 
         # Model forward for initial time-step
         g0, xRec0 = self.embedding_model(xin0, field)
+        normsX = xRec0.swapaxes(1,3).reshape(-1,3)
+        normsX = torch.sqrt(torch.einsum('ij,ij->j',normsX.T, normsX.T))
+        ones = torch.ones((normsX.shape[0])).to(device)
 
-        loss = self.l2 * mseLoss(xin0, xRec0)
+        loss = self.l2 * mseLoss(xin0, xRec0) + self.l4 * mseLoss(normsX, ones)
         loss_reconstruct = loss_reconstruct + self.l1 * mseLoss(xin0, xRec0).detach()
 
         g1_old = g0
@@ -278,12 +289,17 @@ class LandauLifshitzGilbertEmbeddingTrainer(EmbeddingTrainingHead):
             g1Pred = self.embedding_model.koopman_operation(g1_old, field)
             xgRec1 = self.embedding_model.recover(g1Pred)
 
+            normsX = xRec1.swapaxes(1,3).reshape(-1,3)
+            normsX = torch.sqrt(torch.einsum('ij,ij->j',normsX.T, normsX.T))
+            ones = torch.ones((normsX.shape[0])).to(device)
+
             loss = (
                 loss
                 + self.l1 * mseLoss(xgRec1, xin0)
                 + self.l2 * mseLoss(xRec1, xin0)
                 + self.l3
                 * torch.sum(torch.pow(self.embedding_model.koopman_operator, 2))
+                + self.l4 * mseLoss(normsX, ones)
             )
 
 

embedding_trainer.py
@@ -39,6 +39,7 @@ class EmbeddingPhysTrainer(pl.LightningModule):
         self.save_hyperparameters(cfg)
         self.lr = cfg.learning.lr
         self.batch_size = cfg.learning.batch_size_train
+        self.train_path = ''
 
         # dataset
         (
@@ -76,9 +77,11 @@ class EmbeddingPhysTrainer(pl.LightningModule):
             self.model,
             l1=1e1,
             l2=1e1,
-            l3=1e-2
+            l3=1e-2,
+            l4=2,
         )
 
+        self.val_id = 0
         self.losses_train = []
         self.losses_val = []
         self.losses_train_hist = []
@@ -91,9 +94,16 @@ class EmbeddingPhysTrainer(pl.LightningModule):
         cfg = self.hparams
 
         base_path = "C:\\Users\\s174270\\Documents\\datasets\\64x16 field"
-        train_path = "{}\\field_s_state_train_large.h5".format(base_path)
-        val_path = "{}\\field_s_state_test_large.h5".format(base_path)
-        test_path = "{}\\test.h5".format(base_path)
+        # train_path = "{}\\field_s_state_train_large.h5".format(base_path)
+        # val_path = "{}\\field_s_state_test_large.h5".format(base_path)
+        # test_path = "{}\\field_s_state_test_large.h5".format(base_path)
+        train_path = "{}\\field_s_state_train_circ.h5".format(base_path)
+        val_path = "{}\\field_s_state_test_circ.h5".format(base_path)
+        test_path = "{}\\field_s_state_test_circ.h5".format(base_path)
+        # train_path = base_path + "\\field_s_state_train_rest.h5"
+        # val_path = base_path + "\\field_s_state_test_rest.h5"
+        # test_path = base_path + "\\field_s_state_test_rest.h5"
+        self.train_path = train_path
 
         train_set = read_h5_dataset(
             train_path,
@@ -199,6 +209,8 @@ class EmbeddingPhysTrainer(pl.LightningModule):
         return self.step(batch=batch, batch_idx=batch_idx, mode="train")
 
     def validation_step(self, batch, batch_idx):
+        self.val_id = self.val_id + 1
+        self.save_model(filename='val_{}'.format(self.val_id))
         return self.step(batch=batch, batch_idx=batch_idx, mode="val")
 
     def test_step(self, batch, batch_idx):
@@ -246,6 +258,9 @@ class EmbeddingPhysTrainer(pl.LightningModule):
         f.create_dataset('train', data=np.array(self.losses_train_hist))
         f.create_dataset('val', data=np.array(self.losses_val_hist))
         f.close()
+        with open('dataset.txt', 'w') as file:
+            file.write(str(self.train_path))
+        file.close()
 
 
 def train(cfg):
@@ -275,8 +290,8 @@ def train(cfg):
         gpus=cfg.gpus,
         logger=logger,
         num_sanity_val_steps=0,
-        log_every_n_steps=15,
-        check_val_every_n_epoch=25,
+        log_every_n_steps=100,
+        check_val_every_n_epoch=50,
         callbacks=SaveCallback(
             dirpath="{}".format(cfg.embedding.ckpt_path),
             filename=cfg.embedding.display_name,

test_auto_net.py
@@ -15,15 +15,17 @@ from transformer.phys_transformer_gpt2 import PhysformerGPT2
 
 if __name__ == '__main__':
     base = 'C:\\Users\\s174270\\Documents\\datasets\\64x16 field'
-    f = h5py.File(base + '\\field_s_state_test_large.h5')
-    sample_idx = 43
+    # f = h5py.File(base + '\\field_s_state_test_large.h5')
+    f = h5py.File(base + '\\field_s_state_test_rest.h5')
+    # f = h5py.File('./problem4.h5')
+    sample_idx = 7
     sample = np.array(f[str(sample_idx)]['sequence'])
     field = np.array( f[str(sample_idx)]['field'])
     # date = '2022-05-06'
     # time = '22-20-04'
-    date = '00'
-    time = '4'
-    transformer_suffix = '_450'
+    date = '2022-05-10'
+    time = '22-52-57'
+    transformer_suffix = '_100'
     show_losses = True
     init_len = 1
     val_every_n_epoch = 50
@@ -33,15 +35,19 @@ if __name__ == '__main__':
     class Object(object):
         pass
 
+    path = './transformer_output/{}/{}/'.format(date,time)
+    with open(path + 'transformer_config.json', 'r') as file:
+        transformer_cfg = json.loads(json.load(file))
+
     cfg = Object()
     cfg.state_dims = [2, 64, 128]
     cfg.input_dims = [2, 64, 128]
-    cfg.backbone= "ResNet"
-    cfg.backbone_dim = 192
+    cfg.backbone= "Conv"
+    cfg.backbone_dim = 512
     cfg.channels= 5
     cfg.ckpt_path= ""
     cfg.config_name= ""
-    cfg.embedding_dim= 128
+    cfg.embedding_dim= 16
     cfg.fc_dim= 192
     cfg.image_size_x= 64
     cfg.image_size_y= 16
@@ -49,18 +55,15 @@ if __name__ == '__main__':
     model = LandauLifshitzGilbertEmbedding(
         EmmbedingConfig(cfg),
     ).cuda()
-    path = './transformer_output/{}/{}/'.format(date,time)
     model.load_model(path + 'embedder.pth'.format(date,time))
     model.eval()
     for p in model.parameters():
         p.requires_grad = False
 
-    with open(path + 'transformer_config.json', 'r') as file:
-        transformer_cfg = json.loads(json.load(file))
 
     autoregressive = ContinuousTransformerWrapper(
-        dim_in=128,
-        dim_out=128,
+        dim_in=128 if "emb_size" not in transformer_cfg else transformer_cfg["emb_size"],
+        dim_out=128 if "emb_size" not in transformer_cfg else transformer_cfg["emb_size"],
         max_seq_len=transformer_cfg["ctx"],
         attn_layers=Decoder(
             dim=transformer_cfg["decoder_dim"],
@@ -125,15 +128,15 @@ if __name__ == '__main__':
     plt.quiver(recon[-1,0].T, recon[-1,1].T, pivot='mid', color=(0.6,0.0,0.0,0.7))
     plt.show()
     
-    plt.plot(np.arange(sample.shape[0]), np.mean(sample[:,0].reshape(400,-1), axis=1), 'r')
-    plt.plot(np.arange(sample.shape[0]), np.mean(sample[:,1].reshape(400,-1), axis=1), 'g')
-    plt.plot(np.arange(sample.shape[0]), np.mean(sample[:,2].reshape(400,-1), axis=1), 'b')
+    plt.plot(np.arange(sample.shape[0]), np.mean(sample[:,0].reshape(sample.shape[0],-1), axis=1), 'r')
+    plt.plot(np.arange(sample.shape[0]), np.mean(sample[:,1].reshape(sample.shape[0],-1), axis=1), 'g')
+    plt.plot(np.arange(sample.shape[0]), np.mean(sample[:,2].reshape(sample.shape[0],-1), axis=1), 'b')
     # plt.grid()
     # plt.show()
     
-    plt.plot(np.arange(sample.shape[0]), np.mean(recon[:,0].reshape(400,-1), axis=1), 'rx')
-    plt.plot(np.arange(sample.shape[0]), np.mean(recon[:,1].reshape(400,-1), axis=1), 'gx')
-    plt.plot(np.arange(sample.shape[0]), np.mean(recon[:,2].reshape(400,-1), axis=1), 'bx')
+    plt.plot(np.arange(sample.shape[0]), np.mean(recon[:,0].reshape(sample.shape[0],-1), axis=1), 'rx')
+    plt.plot(np.arange(sample.shape[0]), np.mean(recon[:,1].reshape(sample.shape[0],-1), axis=1), 'gx')
+    plt.plot(np.arange(sample.shape[0]), np.mean(recon[:,2].reshape(sample.shape[0],-1), axis=1), 'bx')
     plt.grid()
     plt.show()
     

test_net.py
@@ -10,9 +10,14 @@ from embedding.embedding_landau_lifshitz_gilbert import LandauLifshitzGilbertEmb
 
 if __name__ == '__main__':
     base = 'C:\\Users\\s174270\\Documents\\datasets\\64x16 field'
-    f = h5py.File(base + '\\field_s_state_test_large.h5')
-    sample = np.array(f['4']['sequence'])
-    field = np.array( f['4']['field'])
+    show_losses = True
+    date = '2022-05-11'
+    time = '13-46-21'
+
+    # f = h5py.File(base + '\\field_s_state_test_large.h5')
+    f = h5py.File(base + '\\field_s_state_test_circ.h5')
+    sample = np.array(f['48']['sequence'])
+    field = np.array( f['48']['field'])
     # print(sample.shape)
     # plt.quiver(sample[-1,0].T, sample[-1,1].T, pivot='mid')
     # plt.show()
@@ -35,7 +40,7 @@ if __name__ == '__main__':
     model = LandauLifshitzGilbertEmbedding(
         EmmbedingConfig(cfg),
     ).cuda()
-    model.load_model('C:\\Users\\s174270\\Documents\\transformers4physics\\outputs\\2022-05-02\\22-10-54\\ckpt\\no_name.pth')
+    model.load_model('C:\\Users\\s174270\\Documents\\transformers4physics\\outputs\\{}\\{}\\ckpt\\val_6.pth'.format(date,time))
     model.eval()
     # print(sample.shape)
     sample_t = torch.tensor(sample).float().cuda()
@@ -43,7 +48,34 @@ if __name__ == '__main__':
     field_t[:] = torch.tensor(field)
     a = model.embed(sample_t, field_t)
     recon = model.recover(a)
+
+    # normsX = torch.sqrt(torch.einsum('ij,ij->j',sample_t.swapaxes(1,3).reshape(-1,3).T, sample_t.swapaxes(1,3).reshape(-1,3).T))
+    # normsX = normsX.reshape(-1,16,64).swapaxes(1,2)
+    # sample_t[:,0,:,:] = sample_t[:,0,:,:]/normsX
+    # sample_t[:,1,:,:] = sample_t[:,1,:,:]/normsX
+    # sample_t[:,2,:,:] = sample_t[:,2,:,:]/normsX
+
+    # normsG = torch.sqrt(torch.einsum('ij,ij->j',recon.swapaxes(1,3).reshape(-1,3).T, recon.swapaxes(1,3).reshape(-1,3).T))
+    # normsG = normsG.reshape(-1,16,64).swapaxes(1,2)
+    # recon[:,0,:,:] = recon[:,0,:,:]/normsG
+    # recon[:,1,:,:] = recon[:,1,:,:]/normsG
+    # recon[:,2,:,:] = recon[:,2,:,:]/normsG
+
     recon = recon.detach().cpu().numpy()
+
+    if show_losses:
+        f = h5py.File('C:\\Users\\s174270\\Documents\\transformers4physics\\outputs\\{}\\{}\\losses.h5'.format(date,time), 'r')
+        losses = np.array(f['train'])
+        l = np.arange(len(losses))
+        plt.plot(l,losses)
+        plt.yscale('log')
+        plt.show()
+        losses = np.array(f['val'])
+        l = np.arange(len(losses))
+        plt.plot(l,losses)
+        plt.yscale('log')
+        plt.show()
+
     plt.quiver(sample[0,0].T, sample[0,1].T, pivot='mid', color=(0.0,0.0,0.0,1.0))
     plt.quiver(recon[0,0].T, recon[0,1].T, pivot='mid', color=(0.6,0.0,0.0,0.7))
     plt.show()
@@ -51,14 +83,14 @@ if __name__ == '__main__':
     plt.quiver(recon[-1,0].T, recon[-1,1].T, pivot='mid', color=(0.6,0.0,0.0,0.7))
     plt.show()
     
-    plt.plot(np.arange(sample.shape[0]), np.mean(sample[:,0].reshape(400,-1), axis=1), 'r')
-    plt.plot(np.arange(sample.shape[0]), np.mean(sample[:,1].reshape(400,-1), axis=1), 'g')
-    plt.plot(np.arange(sample.shape[0]), np.mean(sample[:,2].reshape(400,-1), axis=1), 'b')
+    plt.plot(np.arange(sample.shape[0]), np.mean(sample[:,0].reshape(sample_t.size(0),-1), axis=1), 'r')
+    plt.plot(np.arange(sample.shape[0]), np.mean(sample[:,1].reshape(sample_t.size(0),-1), axis=1), 'g')
+    plt.plot(np.arange(sample.shape[0]), np.mean(sample[:,2].reshape(sample_t.size(0),-1), axis=1), 'b')
     # plt.grid()
     # plt.show()
     
-    plt.plot(np.arange(sample.shape[0]), np.mean(recon[:,0].reshape(400,-1), axis=1), 'rx')
-    plt.plot(np.arange(sample.shape[0]), np.mean(recon[:,1].reshape(400,-1), axis=1), 'gx')
-    plt.plot(np.arange(sample.shape[0]), np.mean(recon[:,2].reshape(400,-1), axis=1), 'bx')
+    plt.plot(np.arange(sample.shape[0]), np.mean(recon[:,0].reshape(sample_t.size(0),-1), axis=1), 'rx')
+    plt.plot(np.arange(sample.shape[0]), np.mean(recon[:,1].reshape(sample_t.size(0),-1), axis=1), 'gx')
+    plt.plot(np.arange(sample.shape[0]), np.mean(recon[:,2].reshape(sample_t.size(0),-1), axis=1), 'bx')
     plt.grid()
     plt.show()

test_x_transformers.py
@@ -20,20 +20,21 @@ from util.data_loader import MagDataset
 
 
 if __name__ == "__main__":
-    epochs = 1000
+    epochs = 300
     ctx = 32
-    ndata_train = 500
-    ndata_val = 50
-    stride = 2
-    train_batch_size = 2048
+    ndata_train = -1
+    ndata_val = -1
+    stride = 4
+    train_batch_size = 512
     val_batch_size = 10
     val_every_n_epoch = 50
     save_on_val = True
 
     transformer_cfg = {
         'ctx': ctx,
+        'emb_size': 128,
         'decoder_dim': 512,
-        'depth': 12,
+        'depth': 10,
         'heads': 8,
         'macaron': False,
         "shift_tokens": 0,
@@ -55,8 +56,8 @@ if __name__ == "__main__":
         json.dump(json_data, file)
 
     model = ContinuousTransformerWrapper(
-        dim_in=128,
-        dim_out=128,
+        dim_in=transformer_cfg["emb_size"],
+        dim_out=transformer_cfg["emb_size"],
         max_seq_len=transformer_cfg["ctx"],
         attn_layers=Decoder(
             dim=transformer_cfg["decoder_dim"],
@@ -77,20 +78,21 @@ if __name__ == "__main__":
     cfg = Object()
     cfg.state_dims = [2, 64, 128]
     cfg.input_dims = [2, 64, 128]
-    cfg.backbone = "ResNet"
+    cfg.backbone= "ResNet"
     cfg.backbone_dim = 192
-    cfg.channels = 5
-    cfg.ckpt_path = ""
-    cfg.config_name = ""
-    cfg.embedding_dim = 128
-    cfg.fc_dim = 192
-    cfg.image_size_x = 64
-    cfg.image_size_y = 16
-    cfg.koopman_bandwidth = 7
+    cfg.channels= 5
+    cfg.ckpt_path= ""
+    cfg.config_name= ""
+    cfg.embedding_dim= 128
+    cfg.fc_dim= 192
+    cfg.image_size_x= 64
+    cfg.image_size_y= 16
+    cfg.koopman_bandwidth= 7
     embedding_model = LandauLifshitzGilbertEmbedding(EmmbedingConfig(cfg),)
     embedding_model.load_model(
-        "C:\\Users\\s174270\\Documents\\transformers4physics\\outputs\\2022-05-02\\22-10-54\\ckpt\\no_name.pth"
+        "C:\\Users\\s174270\\Documents\\transformers4physics\\outputs\\2022-05-11\\13-46-21\\ckpt\\val_6.pth"
     )
+    EmmbedingConfig(cfg).to_json_file(path + "embedder_cfg.json")
     torch.save(
         embedding_model.state_dict(),
         path + "embedder.pth"
@@ -147,9 +149,8 @@ if __name__ == "__main__":
 
         return data, batch_size
 
-
     train_set, train_batch_size = read_and_embbed_h5_dataset(
-        "C:\\Users\\s174270\\Documents\\datasets\\64x16 field\\field_s_state_train_large.h5",
+        "C:\\Users\\s174270\\Documents\\datasets\\64x16 field\\field_s_state_train_circ.h5",
         embedding_model,
         block_size=ctx,
         batch_size=train_batch_size,
@@ -165,7 +166,7 @@ if __name__ == "__main__":
         num_workers=1,
     )
     val_set, val_batch_size = read_and_embbed_h5_dataset(
-        "C:\\Users\\s174270\\Documents\\datasets\\64x16 field\\field_s_state_test_large.h5",
+        "C:\\Users\\s174270\\Documents\\datasets\\64x16 field\\field_s_state_test_circ.h5",
         embedding_model,
         block_size=ctx,
         batch_size=val_batch_size,