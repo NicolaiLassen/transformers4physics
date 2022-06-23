b39d4ff267156fca030b1de5fa951804c574b495
Sebastian Tønne Kotewitz <s174270@dtu.dk>
2022-06-23 18:02:56 +0200

    increased size of legend

--- test_embedtrans.py
+++ test_embedtrans.py
@@ -148,6 +148,7 @@ def runit(sample_idx):
         Line2D([0], [0], marker='x', color='blue', label='Mz Model'),
     ]
     plt.legend(handles=legend_elements)
+    plt.setp(plt.gca().get_legend().get_texts(), fontsize='16')
     plt.ylabel('$M_i [-]$', fontsize=32)
     plt.xlabel('$Time [ns]$', fontsize=32)
     plt.xticks(fontsize=24)
@@ -176,6 +177,7 @@ def runit(sample_idx):
         Line2D([0], [0], marker='x', color='blue', label='Mz Model'),
     ]
     plt.legend(handles=legend_elements)
+    plt.setp(plt.gca().get_legend().get_texts(), fontsize='16')
     plt.ylabel('$M_i [-]$', fontsize=32)
     plt.xlabel('$Time [ns]$', fontsize=32)
     plt.xticks(fontsize=24)
